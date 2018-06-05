c85fa76fd98853cb3c51563f60b3c5421053e690
John Doe <dl@progears.ru>
2018-06-05 15:11:01 +0300

    Improvements

---
+++ src/app/store-elements/store-start/store-start.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-store-start',
+  templateUrl: './store-start.component.html',
+  styleUrls: ['./store-start.component.scss']
+})
+export class StoreStartComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/store-elements/store-update/store-update.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-store-update',
+  templateUrl: './store-update.component.html',
+  styleUrls: ['./store-update.component.scss']
+})
+export class StoreUpdateComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
